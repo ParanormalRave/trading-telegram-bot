aa420ef309db8b7223fc6bfccf4132211ccf2f5a
rave <mercyadenubi2@gmail.com>
2026-06-11 12:48:53 +0100

    add error handling to getSession

--- src/lib/session.js
+++ src/lib/session.js
@@ -3,8 +3,16 @@ import { redis } from './redis.js'
 const TTL = 3600 //store an hour worth of time
 // used to get the session id of each chat and return the data in an array form if it exists
 export async function getSession(chatId) {
-  const data = await redis.get(`session:${chatId}`)
-  return data ? JSON.parse(data) : []
+  try{
+    const data = await redis.get(`session:${chatId}`)
+    return data ? JSON.parse(data) : []
+  }catch(error){
+    console.error(`Error message for parsing session ${chatId}`,error.messages )
+    return [];
+  }
+  
+
+  
 }
 
 //use the acquired chat id to save the stringified message and then destruct after an hour of inactivity
