7fee18f0e1d9f909c96b37ddef03d78b555961ee
rave <mercyadenubi2@gmail.com>
2026-06-11 04:34:17 +0100

    redis test check

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,7 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 import { Redis } from '@upstash/redis'
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 })
+
+redis.set('test', 'connected').then(() => console.log('redis connected successfully')).catch((err)=>console.error("Redis failed because",err.message))
