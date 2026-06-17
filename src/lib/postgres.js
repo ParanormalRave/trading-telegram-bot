1428af26a657578b1009f90a15ff1eb6ddd8ebcf
rave <mercyadenubi2@gmail.com>
2026-06-17 05:12:23 +0100

    database fix

--- src/lib/postgres.js
+++ src/lib/postgres.js
@@ -2,24 +2,25 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import pg from 'pg'
+
 const pool = new pg.Pool({
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT || 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-
   max: 10,
   min: 2,
   idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
-pool.on('connect', ()=>{
-  console.log("connected to the database")
-})
+pool.on('connect', () => {
+  console.log('Connected to SparkDB');
+});
 
-pool.on('error', ()=>{
-  console.error("Error:",err);
-})
+pool.on('error', (err) => {
+  console.error('SparkDB connection error:', err);
+});
 
 export const db = pool;
