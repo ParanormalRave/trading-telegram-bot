ad4919b78ff9416a6d7c265fce2fd0ab6e4c4f18
rave <mercyadenubi2@gmail.com>
2026-07-17 03:31:16 +0100

    add more details and a chart

--- src/lib/postgres.js
+++ src/lib/postgres.js
@@ -5,7 +5,7 @@ import pg from 'pg'
 
 const pool = new pg.Pool({
   host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT || 5432,
+  port: process.env.POSTGRES_PORT || 5433,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
