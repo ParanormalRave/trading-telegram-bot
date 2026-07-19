ba74b8345ffd682ae0abc93f264bdb1734a591c0
rave <mercyadenubi2@gmail.com>
2026-07-19 18:15:05 +0100

    database fix

--- src/lib/postgres.js
+++ src/lib/postgres.js
@@ -13,7 +13,7 @@ const pool = new pg.Pool({
   min: 2,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
-  ssl: { rejectUnauthorized: required },
+  ssl: { rejectUnauthorized: true },
 })
 
 pool.on('connect', () => {
